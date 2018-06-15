dd146c2ac1f51d4d2b46a5568951f9f73f573100
doujinstuff <doujinstuffand@gmail.com>
2018-06-14 20:18:39 -0700

    Fix Search Bar

--- app/src/main/java/com/stuff/doujin/h2r/MainActivity.java
+++ app/src/main/java/com/stuff/doujin/h2r/MainActivity.java
@@ -2,7 +2,9 @@ package com.stuff.doujin.h2r;
 
 import android.os.Bundle;
 import android.support.design.widget.NavigationView;
+import android.support.v4.content.ContextCompat;
 import android.support.v4.view.GravityCompat;
+import android.support.v4.view.MenuItemCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
@@ -11,6 +13,7 @@ import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.Toast;
 
 import com.stuff.doujin.h2r.data.Doujin;
@@ -31,7 +34,6 @@ public class MainActivity extends AppCompatActivity
     GetDoujinList getDoujinList;
     GetDoujinDetails getDoujinDetails;
     GetPageList getPageList;
-    SearchView searchView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -48,9 +50,6 @@ public class MainActivity extends AppCompatActivity
 
         NavigationView navigationView = findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
-
-        searchView = findViewById(R.id.search_bar);
-        searchView.setOnQueryTextListener(this);
 
         getDoujinList = new GetDoujinList();
         getDoujinDetails = new GetDoujinDetails();
@@ -81,6 +80,9 @@ public class MainActivity extends AppCompatActivity
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.main, menu);
+        SearchView searchView =
+                (SearchView) menu.findItem(R.id.action_show_search).getActionView();
+        searchView.setOnQueryTextListener(this);
         return true;
     }
 
@@ -91,15 +93,6 @@ public class MainActivity extends AppCompatActivity
         // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
 
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_show_search) {
-            if(searchView.getVisibility() == View.VISIBLE) {
-                searchView.setVisibility(View.GONE);
-            } else {
-                searchView.setVisibility(View.VISIBLE);
-            }
-            return true;
-        }
 
         return super.onOptionsItemSelected(item);
     }
@@ -213,7 +206,6 @@ public class MainActivity extends AppCompatActivity
         String searchQuery = "/hentai-list/search/" + query + "/all/name-az/1/";
         startLoadingFragment();
         getDoujinList.loadDoujinList(this, searchQuery);
-        searchView.setVisibility(View.GONE);
         return false;
     }
 
